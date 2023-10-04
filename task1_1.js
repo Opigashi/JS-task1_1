2baae8bae4203d98826080340e0fd7a1f3a6bf00
Ayaka Ohigashi <a.ohigashi.0129@gmail.com>
2023-10-04 20:00:11 +0900

    first code

--- task1_1.js
+++ task1_1.js
@@ -1,1 +1,16 @@
-//test1
+//3の倍数の時　→ 「”Fizz “ + その数字」
+//5の倍数の時　→ 「”Buzz “ + その数字」
+//それ以外の時　→ 何も処理しない
+//上記を99まで繰り返す
+
+'use strict';
+
+for (let int = 1; int <= 99; i++) {
+    if (int % 3 === 0) {
+        console.log('Fizz ' + int);
+    } else if (int % 5 === 0) {
+        console.log('Buzz ' + int);
+    } else {
+        ;
+    }
+}
